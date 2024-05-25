cc4d3bad5fe8c11761fcbe4e10635b8f9eb5eb6c
alba-garcia09 <albagarlaz@gmail.com>
2024-05-25 11:17:12 +0200

    con algunos comodines

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ function buildPage({ numberOfQuestions, }) {
 
   const button50 = document.createElement('div');
   button50.id = 'button50';
-  button50.setAttribute('class', 'wildcardButon');
+  button50.classList.add('shadow-out', 'button50Ok');
   myHeader.appendChild(button50);
 
   const buttonChange = document.createElement('div');
   buttonChange.id = 'buttonChange';
-  buttonChange.setAttribute('class', 'wildcardButon');
+  buttonChange.classList.add('shadow-out', 'buttonChangeOk');
   myHeader.appendChild(buttonChange);
 
   const buttonCall = document.createElement('div');
   buttonCall.id = 'buttonCall';
-  buttonCall.setAttribute('class', 'wildcardButon');
+  buttonCall.classList.add('shadow-out', 'wildcardButon');
   myHeader.appendChild(buttonCall);
 
   const myMain = document.createElement('main');
@@ -62,7 +62,7 @@ function getRandomNumber({ min, max, }) {
 async function showQuestion(div1, div2, questionData) {
   const myQuestionBox = document.createElement('div');
   myQuestionBox.id = 'myQuestionBox';
-  myQuestionBox.classList.add('myQuestionBox', 'shadow-inner');
+  myQuestionBox.classList.add('myQuestionBox', 'shadow-out');
   myQuestionBox.innerText = questionData.description;
   div1.appendChild(myQuestionBox);
 
@@ -72,7 +72,7 @@ async function showQuestion(div1, div2, questionData) {
     const optionKey = answerOptions[i];
     const myAnswer = document.createElement('div');
     myAnswer.setAttribute('id', optionKey);
-    myAnswer.classList.add('myAnswerBox', 'shadow-inner');
+    myAnswer.classList.add('myAnswerBox', 'shadow-out');
     myAnswer.innerText = `${optionKey}) ${questionData.answers[optionKey]}`;
 
     div2.appendChild(myAnswer);
@@ -83,7 +83,7 @@ function getIndexLevel(item) {
   const myLevels = item.children;
   for (let i = 0; i < myLevels.length; i++) {
     if (myLevels[i].classList.contains('nextLevelBox')) {
-      const myCurrentLevel=parseInt(myLevels[i].innerText);
+      const myCurrentLevel = parseInt(myLevels[i].innerText);
       return myCurrentLevel;
     }
   }
@@ -105,7 +105,7 @@ function giveLevel(item) {
 }
 
 //  La función que pasa de nivel cambia de color y elimina a los hijos de question y de answer
-async function nextLevel({ circle, questionIds,}) {
+async function nextLevel({ circle, questionIds, }) {
   const myQuestionContainer = document.getElementById('myQuestionContainer');
   const myAnswerContainer = document.getElementById('myAnswerContainer');
   const numberLevel = getIndexLevel(circle);
@@ -117,7 +117,7 @@ async function nextLevel({ circle, questionIds,}) {
   const myLevels = document.getElementById('myLevelContainer').children;
   newLevel.setAttribute('class', 'nextLevelBox');
 
-  for (let i = (numberLevel-1) ; i >= 0; i--) {
+  for (let i = (numberLevel - 1); i >= 0; i--) {
     myLevels[i].classList.add('class', 'guessedLevelBox');
     myLevels[i].classList.remove('nextLevelBox');
   }
@@ -125,8 +125,145 @@ async function nextLevel({ circle, questionIds,}) {
   getQuestion({ questionIds, });
 }
 
+// function getRandomNumberForConfeti(min, max) {
+//   const randomNumber = Math.floor(Math.random() * (max - min) + min);
+//   return randomNumber;
+// }
+
+// function getConfetti(colors) {
+//   const root = document.getElementById('root');
+
+//   const confettiContainer = document.createElement('div');
+//   confettiContainer.classList.add('confetti-container');
+//   root.appendChild(confettiContainer);
+
+//   const confetti = document.createElement('div');
+//   confetti.classList.add('confetti');
+//   confettiContainer.appendChild(confetti);
+
+//   for (let i = 0; i < 100; i++) {
+//     const randomColorIndex = getRandomNumber(0, colors.length);
+//     const randomSpeed = getRandomNumberForConfeti(15, 50);
+//     const confettiItem = document.createElement('i');
+//     confettiItem.classList.add('rectangle');
+//     confettiItem.classList.add(colors[randomColorIndex]);
+//     // confettiItem.style['--bg'] = colors[randomColorIndex];
+//     // confettiItem.style['--speed'] = randomSpeed;
+//     confettiItem.style['animation-duration'] = `calc(70s / ${randomSpeed})`;
+//     confetti.appendChild(confettiItem);
+//   }
+// }
+
+function lastWindow({ circle, item, }) {
+  const numberLevel = getIndexLevel(circle);
+
+  const myMain = document.getElementById('myMain');
+  const myPanel = document.createElement('div');
+  myPanel.classList.add('shadow-bigger');
+  myPanel.id = 'myPanel';
+  myMain.appendChild(myPanel);
+
+  const myTitle = document.createElement('div');
+  myTitle.id = 'myTitle';
+  myPanel.appendChild(myTitle);
+
+  const myCloseButton = document.createElement('div');
+  myCloseButton.id = 'myCloseButton';
+  myCloseButton.innerText = 'Vuelve a jugar';
+  myCloseButton.classList.add('closeButton');
+  myPanel.appendChild(myCloseButton);
+
+  function myClickCallback(event) {
+    const root = document.getElementById('root');
+    const myMain = document.getElementById('myMain');
+    const myHeader = document.getElementById('myHeader');
+
+    root.removeChild(myMain);
+    root.removeChild(myHeader);
+    runPage();
+  };
+
+  myCloseButton.addEventListener('click', myClickCallback);
+
+  if (item.classList.contains('myWrongBox')) {
+    myTitle.innerText = 'Has perdido';
+    return;
+
+  } else if (item.classList.contains('myGuessedBox') && numberLevel === 15) {
+    myTitle.innerText = 'Has ganado';
+    // getConfetti(['red', 'blue', 'green', 'yellow', 'white', 'pink',]);
+    return;
+
+  } else {
+    myMain.removeChild(myPanel);
+    return;
+  }
+}
+
+function changeWildcard({ questionIds, }) {
+  console.log('he entrado en la funcion de change');
+  const myQuestionContainer = document.getElementById('myQuestionContainer');
+  const myAnswerContainer = document.getElementById('myAnswerContainer');
+
+  function myClickCallback(event) {
+    const buttonChange = document.getElementById('buttonChange');
+    if (buttonChange.classList.contains('buttonChangeDone')) {
+      alert('Has usado el comodin ya');
+      return;
+    }
+
+    myQuestionContainer.innerHTML = '';
+    myAnswerContainer.innerHTML = '';
+    getQuestion({ questionIds, });
+    buttonChange.classList.add('buttonChangeDone');
+    buttonChange.classList.remove('buttonChangeOk');
+  }
+
+  const clonedButtonChange = buttonChange.cloneNode(true);
+  clonedButtonChange.addEventListener('click', myClickCallback);
+  buttonChange.parentElement.replaceChild(clonedButtonChange, buttonChange);
+}
+
+async function fiftyWildcard({questionData}) {
+  const correctAnswer = await questionData.correctAnswer;
+  console.log('my correctAnswer', correctAnswer);
+
+  const myAnswerContainer = document.getElementById('myAnswerContainer').children;
+
+  function myClickCallback(event) {
+    const button50 = document.getElementById('button50');
+    if (button50.classList.contains('button50Done')) {
+      alert('Has usado el comodín ya');
+      return;
+    }
+
+    const removedAnswersArray = [];
+    console.log(removedAnswersArray);
+    while (removedAnswersArray.length < 2) {
+      const indexForRemove = getRandomNumber({ min: 0, max: 4, });
+      console.log('indexinrandomnumber', indexForRemove);
+      const myAnswer = myAnswerContainer[indexForRemove];
+      console.log('myAnswer', myAnswer);
+
+      if (correctAnswer !== myAnswer.id) {
+        button50.classList.add('button50Done');
+        button50.classList.remove('button50Ok');
+        removedAnswersArray.push(myAnswer);
+        myAnswer.classList.add('myFilterBox');
+
+        const clonedMyAnswer = myAnswer.cloneNode(true);
+        myAnswer.parentElement.replaceChild(clonedMyAnswer, myAnswer);
+      }
+    }
+  }
+
+  const clonedButton50 = button50.cloneNode(true);
+  clonedButton50.addEventListener('click', myClickCallback);
+  button50.parentElement.replaceChild(clonedButton50, button50);
+}
+
 // Añadir lo  de que muestre la correcta y que no se puedan clicarmás una vez clicados los naranjas, y que no se puedan clicar ya mas, además que vuelva a enseñar las preguntas
-async function toGuess({ item, questionData, circle, questionIds,}) {
+async function toGuess({ item, questionData, circle, questionIds, }) {
   const correctAnswer = await questionData.correctAnswer;
   console.log(correctAnswer);
   const dadElement = item.parentElement;
@@ -154,7 +291,8 @@ async function toGuess({ item, questionData, circle, questionIds,}) {
     } else if (item.classList.contains('myTryingrBox') && item.id === correctAnswer) {
       item.classList.add('myGuessedBox');
       item.classList.remove('myTryingrBox');
-      setTimeout(() => nextLevel({ circle, questionIds,}), 3000);
+      setTimeout(() => nextLevel({ circle, questionIds, }), 3000);
+      setTimeout(() => lastWindow({ circle, item, }), 5000);
 
     } else {
       item.classList.add('myWrongBox');
@@ -164,11 +302,12 @@ async function toGuess({ item, questionData, circle, questionIds,}) {
       myCorrectBox.classList.add('myGuessedBox');
       myCorrectBox.classList.remove('myTryingrBox');
 
-      const numberLevel= getIndexLevel(circle);
-      const myLevelBox= document.getElementById(`levelBox${numberLevel}`);
+      const numberLevel = getIndexLevel(circle);
+      const myLevelBox = document.getElementById(`levelBox${numberLevel}`);
 
       myLevelBox.classList.add('class', 'wrongLevelBox');
       myLevelBox.classList.remove('nextLevelBox');
+      setTimeout(() => lastWindow({ circle, item, }), 5000);
 
       // al final es lo mismo pero menos lineas de la manera que dejo
       // const findIndexCallback = (child) => child.id === correctAnswer;
@@ -179,11 +318,6 @@ async function toGuess({ item, questionData, circle, questionIds,}) {
   });
   return;
 }
-
-
-// async function name(params) {
-
-// }
 
 //  Aqui vamos mostrando todo
 async function getQuestion({ questionIds, }) {
@@ -212,9 +346,13 @@ async function getQuestion({ questionIds, }) {
     const myAnswers = myAnswerContainer.children;
 
     for (let i = 0; i < myAnswers.length; i++) {
-      toGuess({ item: myAnswers[i], questionData: myInfo, circle: myLevelContainer, questionIds,});
+      toGuess({ item: myAnswers[i], questionData: myInfo, circle: myLevelContainer, questionIds, });
     }
   }
+
+  // wilcards here
+  changeWildcard({ questionIds, });
+  fiftyWildcard({questionData: myInfo, });
 }
 
 async function runPage() {
